10a115d631edbe71cec731fee5976ff760763706
chiprek <chiprek.cs@gmail.com>
2026-01-21 18:17:32 -0600

    Updated app.py

--- src/btask/app.py
+++ src/btask/app.py
@@ -3,10 +3,18 @@ from textual.containers import Container, Horizontal, Vertical
 from textual.widgets import DataTable, Footer, Header, Label, ListItem, ListView, Log
 
 
-class Sidebar(Container):
+class ProjectDetails(Container):
     def compose(self) -> ComposeResult:
-        with Horizontal():
-            yield Label("Work orders")
+        with DataTable(show_header=True, fixed_rows=10):
+            yield Header()
+
+
+class Sidebar(Container):
+    TITLE = "Work_Orders"
+
+    def compose(self) -> ComposeResult:
+        yield Label("Work Orders", id="work-orders")
+        yield ListView(ListItem(Label("example")), ListItem(Label("shiiiiiiiiii")))
 
     def log_to_sidebar(self, msg: str) -> None:
         self.query_one(Log).write_line(msg)
@@ -16,11 +24,16 @@ class BTaskApp(App[None]):
     CSS_PATH = "btask.tcss"
     TITLE = "btask"
 
-    BINDINGS = [("crtl+w", "toggle_sidebar", "Toggle Sidebar")]
+    BINDINGS = [("w", "toggle_sidebar", "Toggle Sidebar")]
 
     def compose(self) -> ComposeResult:
-        yield Sidebar(classes="-hidden")
         yield Header()
+        with Horizontal():
+            yield Sidebar(
+                classes="-hidden",
+            )
+            with Vertical():
+                yield ProjectDetails()
         yield Footer()
 
     def action_toggle_sidebar(self) -> None:
