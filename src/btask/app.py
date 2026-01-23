257c5c624f4df9b870bdcd3d4a752463ef0fb973
chiprek <chiprek.cs@gmail.com>
2026-01-22 18:57:55 -0600

    updated app.py

--- src/btask/app.py
+++ src/btask/app.py
@@ -1,12 +1,28 @@
 from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal, Vertical
-from textual.widgets import DataTable, Footer, Header, Label, ListItem, ListView, Log
+from textual.widgets import (
+    Button,
+    DataTable,
+    Footer,
+    Header,
+    Label,
+    ListItem,
+    ListView,
+    Log,
+)
 
 
 class ProjectDetails(Container):
     def compose(self) -> ComposeResult:
-        with DataTable(show_header=True, fixed_rows=10):
-            yield Header()
+        with Container(id="view-port"):
+            yield Label("Place-holder", id="place-holder")
+            with Vertical(
+                name="view-port-footer", id="view-port-footer", classes="-hidden"
+            ):
+                yield Button(label="yes", id="view-port-button")
+
+    def log_to_ProjectDetails(self, msg: str) -> None:
+        self.query_one(Log).write_line(msg)
 
 
 class Sidebar(Container):
@@ -24,7 +40,10 @@ class BTaskApp(App[None]):
     CSS_PATH = "btask.tcss"
     TITLE = "btask"
 
-    BINDINGS = [("w", "toggle_sidebar", "Toggle Sidebar")]
+    BINDINGS = [
+        ("w", "toggle_sidebar", "Toggle Sidebar"),
+        ("m", "toggle_project_details", "toggle project details"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -38,3 +57,6 @@ class BTaskApp(App[None]):
 
     def action_toggle_sidebar(self) -> None:
         self.query_one(Sidebar).toggle_class("-hidden")
+
+    def action_toggle_ProjectDetails_menu(self) -> None:
+        self.query_one(ProjectDetails).toggle_class("view-port-footer.-hidden")
