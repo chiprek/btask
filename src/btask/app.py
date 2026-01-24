0715b1f7cf149c89f51f15ed886bacddc41fba15
chiprek <chiprek.cs@gmail.com>
2026-01-24 14:43:48 -0600

    Refactored app.py

--- src/btask/app.py
+++ src/btask/app.py
@@ -1,51 +1,17 @@
 from textual.app import App, ComposeResult
-from textual.containers import Container, Horizontal, Vertical
+from textual.containers import Horizontal, Vertical
 from textual.widgets import (
-    Button,
-    DataTable,
     Footer,
     Header,
-    Label,
-    ListItem,
-    ListView,
-    Log,
-    ProgressBar,
 )
 from total import get_total
-
-
-class PD_Menu(Container):
-    def compose(self) -> ComposeResult:
-        with Container():
-            yield Button(label="yes", id="view-port-button")
-            with Horizontal():
-                yield ProgressBar(total=get_total())
-
-
-class ProjectDetails(Container):
-    def compose(self) -> ComposeResult:
-        with Container(id="view-port"):
-            yield Label("Place-holder", id="place-holder")
-            with Vertical():
-                yield PD_Menu(classes="-hidden")
-
-    def log_to_ProjectDetails(self, msg: str) -> None:
-        self.query_one(Log).write_line(msg)
-
-
-class Sidebar(Container):
-    TITLE = "Work_Orders"
-
-    def compose(self) -> ComposeResult:
-        yield Label("Work Orders", id="work-orders")
-        yield ListView(ListItem(Label("example")), ListItem(Label("shiiiiiiiiii")))
-
-    def log_to_sidebar(self, msg: str) -> None:
-        self.query_one(Log).write_line(msg)
+from widgets.pd_menu import PD_Menu
+from widgets.project_details import ProjectDetails
+from widgets.sidebar import Sidebar
 
 
 class BTaskApp(App[None]):
-    CSS_PATH = "btask.tcss"
+    CSS_PATH = "btask.css"
     TITLE = "btask"
 
     BINDINGS = [
@@ -67,4 +33,4 @@ class BTaskApp(App[None]):
         self.query_one(Sidebar).toggle_class("-hidden")
 
     def action_toggle_pd_menu(self) -> None:
-        self.query_one(PD_Menu).toggle_class("-hidden")
+        self.query_one("#pd-menu").toggle_class("-hidden")
