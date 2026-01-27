4f12d7e69cab16878f42cbcd861fe9cecacec6fd
chiprek <chiprek.cs@gmail.com>
2026-01-27 17:58:03 -0600

    Updated project details.py

--- src/btask/widgets/project_details.py
+++ src/btask/widgets/project_details.py
@@ -9,7 +9,7 @@ from .pd_menu import PD_Menu
 class ProjectDetails(Container):
     def compose(self) -> ComposeResult:
         with Container(id="view-port"):
-            yield Label("Place-holder", id="place-holder")
+            yield Label("Project Details", id="place-holder")
             yield DataTable(id="kits-table")
             with Vertical():
                 yield PD_Menu(id="pd-menu", classes="-hidden")
@@ -20,6 +20,7 @@ class ProjectDetails(Container):
 
         if not project:
             return
+        # update the name of the menu
         placeholder = self.query_one("#place-holder", Label)
         placeholder.update(f"Project: {project['name']}")
 
@@ -27,7 +28,7 @@ class ProjectDetails(Container):
         table.clear()
 
         if not table.columns:
-            table.add_columns("Kit Name", "Status", "Quantity", "Notes")
+            table.add_columns("Kit Name", "Status", "Quantity", "Notes", "completed")
         kits = project.get("Kits", [])
 
         if kits:
@@ -37,6 +38,7 @@ class ProjectDetails(Container):
                     kit.get("status", ""),
                     str(kit.get("quantity", 0)),
                     kit.get("notes", ""),
+                    kit.get("Completed", False),
                 )
         else:
-            table.add_row("No kits added", "-", "-", "-")
+            table.add_row("No kits added", "-", "-", "-", "-")
