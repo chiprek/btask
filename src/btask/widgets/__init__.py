41b727f17c5670aaf9dea08dd2c9182ff51f582a
chiprek <chiprek.cs@gmail.com>
2026-02-02 21:45:51 -0600

    Updated __init__.py

--- src/btask/widgets/__init__.py
+++ src/btask/widgets/__init__.py
@@ -1,5 +1,7 @@
+from .add_kit_dialog import AddKitDialog
 from .pd_menu import PD_Menu
+from .pin_prompt import PinPrompt
 from .project_details import ProjectDetails
 from .sidebar import Sidebar
 
-__all__ = ["Sidebar", "ProjectDetails", "PD_Menu"]
+__all__ = ["Sidebar", "ProjectDetails", "PD_Menu", "PinPrompt", "AddKitDialog"]
