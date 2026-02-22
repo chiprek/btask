3013e2d2cd5acc24eebbea0e399b375672d6d980
chiprek <chiprek.cs@gmail.com>
2026-02-21 21:38:13 -0600

    update archive_project_dialog.py

--- src/btask/widgets/archive_project_dialog.py
+++ src/btask/widgets/archive_project_dialog.py
@@ -1,7 +1,14 @@
+from textual import on
 from textual.screen import ModalScreen
+from textual.app import ComposeResult
+from textual.containers import Container, Vertical
+from textual.widgets import Button, Label, Select
 
 
 class ArchiveProjectDialog(ModalScreen):
     def __init__(self, projects: list):
         super().__init__()
         self.projects = projects
+
+    def compose(self) -> ComposeResult:
+        with Container()
